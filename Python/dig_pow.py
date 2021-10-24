c752eb6f55f989d813063b508a6cc4bf73c7c608
theLiuk23 <ldvcoding@gmail.com>
2021-10-24 22:30:51 +0200

    added comments

--- Python/dig_pow.py
+++ Python/dig_pow.py
@@ -1,23 +1,24 @@
-import timeit
-
 # it checks if it exists any solutions of k so that:
-# a ^ p + b ^ (p+1) + c ^(p+2) + d ^ (p+3) + ...) = n * k
+# a ^ p + b ^ (p+1) + c ^(p+2) + d ^ (p+3) + ... = n * k
 def dig_pow(n, p):
     sum = 0
     digits = []
 
+    # gets single digits of n
     for digit in str(n):
         digits.append(int(digit))
 
+    # get the sum (a ^ p + b ^ (p+1) + c ^(p+2) + d ^ (p+3) + ...)
     for i in range(len(digits)):
         sum += digits[i]**(p+i)
 
+    # if sum / n returns an integer, we have found a special number
     if sum % n == 0:
         return sum // n
 
+    # if not return -1
     else:
         return -1
 
 
-timeit.timeit()
 print(dig_pow(33, 45))
