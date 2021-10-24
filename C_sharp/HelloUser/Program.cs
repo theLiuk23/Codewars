ff1bf51e96bc83b0c42bdf9f0ca4c99b7fca7478
theLiuk23 <ldvcoding@gmail.com>
2021-10-24 21:59:47 +0200

    created new c-sharp script called 'HelloUser'

---
+++ C_sharp/HelloUser/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloUser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Environment.UserName;
+            Console.WriteLine("Hello, " + username + "!");
+        }
+    }
+}
