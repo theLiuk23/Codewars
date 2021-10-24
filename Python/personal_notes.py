8c2e8236f2632ba33ce0cf0c0ea61b1eed291a16
theLiuk23 <ldvcoding@gmail.com>
2021-10-24 20:08:15 +0200

    commit4

---
+++ Python/personal_notes.py
@@ -0,0 +1,10 @@
+def add_binary(a,b):
+    return '{0:o}'.format(a + b)
+
+
+def add_binary2(a,b):
+    """Adds a and b together and returns a binary string"""
+    return bin(a + b)[2::]
+
+
+print(add_binary2(6, 5))
