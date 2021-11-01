a2a59c656fd4a230431024ad7c311e769c13abaf
theLiuk23 <ldvcoding@gmail.com>
2021-11-01 19:42:04 +0100

    edited scramble_strings.py

--- Python/scramble_strings.py
+++ Python/scramble_strings.py
@@ -1,21 +1,18 @@
 import timeit
 
 # abcxyz - cab ==> True
+# aaaabc - abc
 # s1     - s2
 
 def scramble(s1, s2):
-    index = 0
-    s1 = sorted(s1)
-    s2 = sorted(s2)
-
     if s1 == s2:
         return True
 
     for letter in s2:
         if s1.__contains__(letter):
-            s1 = remove(s1, index)
-            s2 = remove(s2, index)
-        else:
+            s1 = remove(s1, s1.index(letter))
+            s2 = remove(s2, s2.index(letter))
+        if not s1.__contains__(letter):
             return False
 
     return True
@@ -29,21 +26,20 @@ def remove(s, index):
 
 
 
-print(str(scramble('rkqodlw', 'world')))
-print(str(scramble('cedewaraaossoqqyt', 'codewars')))
-print(str(scramble('katas', 'steak')))
-print(str(scramble('scriptingjava', 'javascript')))
-print(str(scramble('scriptjava', 'javascript')))
+print(timeit.timeit("scramble('rkqodlw', 'world')", setup="from __main__ import scramble"))
 
 
 
 
 # SOLUTION:
 
-def scramble(s1,s2):
+def scramble2(s1,s2):
     for c in set(s2):
         # string.count(char) returns the num of char in the string.
         # e.g.= in "hello" "hello".count(l) ==> 2
         if s1.count(c) < s2.count(c):
             return False
     return True
+
+
+print(str(timeit.timeit("scramble2('rkqodlw', 'world')", setup="from __main__ import scramble2")))
