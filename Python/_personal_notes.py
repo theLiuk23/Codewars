2fd7b25ac6e1e3d2b20ed59f741bc98fd30d1627
theLiuk23 <ldvcoding@gmail.com>
2021-11-04 21:02:18 +0100

    changed personal_notes.py

--- Python/_personal_notes.py
+++ Python/_personal_notes.py
@@ -1,21 +1,27 @@
 ### file to test some functions
+### and write some notes
 ### pls ignore it
+
+import add_binary
+
 
 
 
 # functions from codewars (add_binary kata)
-def add_binary(a,b):
-    return '{0:o}'.format(a + b)
-
-
-def add_binary2(a,b):
-    """Adds a and b together and returns a binary string"""
-    return bin(a + b)[2::]
-
-
-print(add_binary2(6, 5))
+# def add_binary(a,b):
+#     return '{0:o}'.format(a + b)
 
 
 
 
-# next
+# def add_binary2(a,b):
+#     """Adds a and b together and returns a binary string"""
+#     return bin(a + b)[2::]
+
+
+
+
+# use of __main__
+if __name__ == '__main__':
+    print(add_binary.add_binary(1, 2))
+    print("personale_notes module name: {}".format(__name__))
--- Python/add_binary.py
+++ Python/add_binary.py
@@ -2,17 +2,11 @@ import sys
 
 def add_binary(a, b):
     sum = a + b
-    print("max: " + str(sys.maxsize))
-    print(type(sum))
     result = ""
     while True:
         result += str(sum % 2)
-        # print("sum: " + str(sum) + "; result: " + result)
         sum = int(sum // 2)
-        # print(str(sum))
         if sum == 0:
             break
 
     return "".join(reversed(result))
-
-print(add_binary(6906393867515314436669271, 1375110409054115922127037))
