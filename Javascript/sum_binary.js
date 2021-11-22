975944b1447b0001fdbd6b00ab0e53d1ae0783d0
theLiuk23 <ldvcoding@gmail.com>
2021-11-22 18:00:31 +0100

    nodejs npm installation

--- Javascript/sum_binary.js
+++ Javascript/sum_binary.js
@@ -1,18 +1,18 @@
 var countBits = function(n) {
-    let sum = 0;
-    let binary = int((n >>> 0).toString(2));
+   let sum = 0;
+   let binary = int((n >>> 0).toString(2));
     
-    // console.log(binary.toString());
-    for (var i=0; i < binary.length; i++)
-    {
-       if (binary[i] == 1)
-       {
-          sum += 1; 
-       }
-    }
+   // console.log(binary.toString());
+   for (var i=0; i < binary.length; i++)
+   {
+      if (binary[i] == 1)
+      {
+         sum += 1;
+      }
+   }
     
-    // console.log(sum.toString());
-    return sum;
+   // console.log(sum.toString());
+   return sum;
 };
 
 console.log(countBits(123432).toString());
