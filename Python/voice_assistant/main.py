b3c6c6445225fc17a907c31058567f8ab928822f
Liuk23 <ldvcoding@gmail.com>
2021-12-08 23:04:32 +0100

    fatto

--- Python/voice_assistant/main.py
+++ Python/voice_assistant/main.py
@@ -1,41 +1,78 @@
+import urllib.request
+import re
+import webbrowser
 import speech_recognition as sr
 import pyttsx3
+import sys
+import webbrowser
+import pywhatkit
 
-
-def change_voice(engine, index=0):
-    engine.setProperty('voice', voices[index].id)
-    return True
 
 
 # initializators
 rec = sr.Recognizer()
 engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
 
 # variables
-current_voice = engine.getProperty('voice')
 voices = engine.getProperty('voices')
-change_voice(engine, 0) #sets language to italian
+assistant_name = ''
+commands_filename = 'commands.txt'
+commands_list = []
 
+
+class SearchOnYt():
+    def Main(self, command):
+        command = command.replace('mozart', 'banane')
+        print("ARG: " + command)
+        pywhatkit.playonyt(command)
+
+
+with open(commands_filename) as file:
+    lines = file.readlines()
+    lines = [line.rstrip() for line in lines]
+    for line in lines:
+        args = line.split('/')
+        commands_list.append([args[0], args[1]])
 
 
 # prints avaible languages in the os
-index = 0
-for voice in voices:
-   print(f'index-> {index} -- {voice.name}')
-   index +=1
+# index = 0
+# for voice in voices:
+#    print(f'index-> {index} -- {voice.name}')
+#    index +=1
 
 
 def Speak(text):
     engine.say(text)
     engine.runAndWait()
+    return True
+
+
+def RunCommand(command):
+    filename = ''
+    for index, result in enumerate(commands_list):
+        if result.__contains__(command):
+            filename = commands_list[index][1]
+            break
+    SearchOnYt().Main(command)
 
 
 
-while True:
-    with sr.Microphone() as mic:
-        print('listening')
-        voice = rec.listen(mic)
-        command = rec.recognize_google(voice)
+def Listening():
+    while True:
+        with sr.Microphone() as mic:
+            print('listening')
+            voice = rec.listen(mic)
+            command = rec.recognize_google(voice, language='it-IT').lower()
+            print('command: {}'.format(command))
+            if command.__contains__(assistant_name):
+                RunCommand(command.replace(assistant_name + " ", ''))
 
 
-Speak("ciao, sono italiano e mi piace molto la pasta col sugo. Viva la mafia e il duce!!!")
+
+
+if __name__ == "__main__":
+    Listening()
