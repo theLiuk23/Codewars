aeb905b5965d3c7ada32e63ccaccf13e3840f65e
theLiuk23 <ldvcoding@gmail.com>
2021-12-27 11:31:45 +0100

    added console and user-inputs to quadratic_equation.py

--- Python/quadratic_equation.py
+++ Python/quadratic_equation.py
@@ -1,4 +1,5 @@
 import math
+import sys
 
 def quadratic_equation(a = 0, b = 0, c = 0):
     if a == 0:
@@ -14,7 +15,36 @@ def quadratic_equation(a = 0, b = 0, c = 0):
 
     return x1, x2
 
+def main():
+    try:
+        a = int(input("a: "))
+        b = int(input("b: "))
+        c = int(input("c: "))
+    except ValueError:
+        print('You input in a wrong manner.')
+        console()
+        return
+    except:
+        sys.exit(1)
 
-# add here three nums as arguments
-soltions = quadratic_equation(1, 3, 1)
-print("x1: " + str(soltions[0]) + "\nx2: " + str(soltions[1]))
+    soltions = quadratic_equation(a, b, c)
+    print("x1: " + str(soltions[0]) + "\nx2: " + str(soltions[1]))
+
+
+def console():
+    try:
+        command = str(input('console: '))
+        if command == 'e':
+            sys.exit(0)
+        elif command == 'r':
+            main()
+        else:
+            print(f'{command} is not a command for this script ({__name__}).')
+            console()
+    except:
+        sys.exit(1)
+
+
+
+if __name__ == "__main__":
+    console()
